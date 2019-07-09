f9db903ff82d661fa225c3fb89acad5d1c04938b
Erick Pacheco Pedraza <eum602@gmail.com>
2019-07-09 10:06:44 -0500

    dispatching actions

--- redux-basics.js
+++ redux-basics.js
@@ -6,16 +6,39 @@ const initialState = { //this can also be a number or whatever
 }
 
 //Reducer
-const rootReducer = (state = initialState, action) => { //state:current state to update
-    //state is initialized to initialState object(if that is not defined)
-    return state //it now does not do nothing yet
-}
+    const rootReducer = (state = initialState, action) => { //state:current state to update
+        //state is initialized to initialState object(if that is not defined)
+        if(action.type==='INC_COUNTER'){
+            return {
+                ...state,
+                counter:state.counter + 1
+            }
+        }
+        if(action.type==='ADD_COUNTER'){
+            return {
+                ...state,
+                counter:state.counter + action.value //incrementing the value we sent to add
+            }
+        }
+        return state //it now does not do nothing yet
+    }
 
 //Store
-const store = createStore(rootReducer)//store must be initialized with a reducer
-//the reducer is the only one connected to the state, that is why we initialize with a reducer
-console.log(store.getState()) //will return undefined unless we initialize the state
+    const store = createStore(rootReducer)//store must be initialized with a reducer
+    //the reducer is the only one connected to the state, that is why we initialize with a reducer
+    console.log(store.getState()) //will return undefined unless we initialize the state
 
-//Dispatching
+
+//Dispatching action
+    store.dispatch({type: 'INC_COUNTER'}) //argument is a javascript object which represents the action
+    //use UPPERCASE for type
+
+    //you can add other properties like{type:'ADD_COUNTER', value:..., name:...}
+    //or also you can add the payload:
+    //store.disptach({type:'ADD_COUNTER',payload:{}})//payload is a javascript object
+    store.dispatch({type: 'ADD_COUNTER',value:10})
+
+    console.log(store.getState())
+
 
 //Subscription
