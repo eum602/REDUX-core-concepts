983da597aa9e893375ae3a1587ab17980bb828b5
Erick Pacheco Pedraza <eum602@gmail.com>
2019-07-09 10:43:26 -0500

    added redux library -created STORE and REDUCER in REACT

---
+++ src/Store/reducer.js
@@ -0,0 +1,8 @@
+const initialState =  {
+    counter: 0
+}
+const reducer = (state,action) =>{//defining the reducer
+    return state    
+}
+
+export default reducer
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux'
+import reducer from './Store/reducer'
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducer)//creating the store, reducer is a must
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
